99e80c30ac801a467322626dc585a819fd0c1c1d
Rohit Jadhav <rj6228442@gmail.com>
2025-07-05 17:27:54 +0530

    Update shopping_cart.py

--- Python_projects/Shopping cart/shopping_cart.py
+++ Python_projects/Shopping cart/shopping_cart.py
@@ -1,1 +1,25 @@
+# Shopping cart system
 
+foods = []  #using list because it is changeable
+prices = []
+total = 0
+
+while True:
+  food = input("Enter your food(q to quit): ")
+  if food.lower() == "q":
+    break
+  else:
+    price = float(input(f"Enter price of {food}: Rs. "))
+    foods.append(food)
+    prices.append(price)
+
+print("----- YOUR CART -----")
+
+for food in foods:
+  print(food)
+
+for price in prices:
+  total += price
+
+print("---------------------")
+print(f"Your total is Rs. {total}")
