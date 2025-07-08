aa717ad234a869dce808d2848c1942dbd0b38475
Rohit Jadhav <rj6228442@gmail.com>
2025-07-08 15:05:58 +0530

    added play again feature

--- Python_projects/Rock, Paper, Scissors/rock_paper_scirssors.py
+++ Python_projects/Rock, Paper, Scissors/rock_paper_scirssors.py
@@ -5,21 +5,36 @@ options = ("rock","paper","scissors")
 player = None
 computer = random.choice(options)
 
-while player not in options:
- player = input("Throw your weapon: ")
+running = True
+
+while running:
+
+  player = None
+  computer = random.choice(options)
+  while player not in options:
+   player = input("Throw your weapon (rock/paper/scissors): ")
 
 
-print(f"Player: {player}")
-print(f"Computer: {computer}")
+  print(f"Player: {player}")
+  print(f"Computer: {computer}")
 
 
-if player == computer:
- print("It's a tie!!")
-elif player == "rock" and computer == "scissors":
- print("You WIN!!")
-elif player == "paper" and computer == "rock":
- print("You WIN!!")
-elif computer == "scissors" and player == "paper":
- print("You WIN!!")
-else:
- print("You LOOSE!!")
+  if player == computer:
+    print("It's a tie!!")
+  elif player == "rock" and computer == "scissors":
+    print("You WIN!!")
+  elif player == "paper" and computer == "rock":
+    print("You WIN!!")
+  elif computer == "scissors" and player == "paper":
+    print("You WIN!!")
+  else:
+    print("You LOSE!!")
+
+  play_again = input("Wanna play again (y/n): ")
+  if not play_again == "y":
+    running = False
+
+print("Thanks for playing!")
+
+
+ 
