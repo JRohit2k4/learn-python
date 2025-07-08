4a4c8a08225fe2045d58c127b92b8eb35808a3fa
Rohit Jadhav <rj6228442@gmail.com>
2025-07-08 11:52:29 +0530

    created rock_paper_scirssors.py

--- Python_projects/Rock, Paper, Scissors/rock_paper_scirssors.py
+++ Python_projects/Rock, Paper, Scissors/rock_paper_scirssors.py
@@ -1,1 +1,25 @@
+import random
 
+options = ("rock","paper","scissors")
+
+player = None
+computer = random.choice(options)
+
+while player not in options:
+ player = input("Throw your weapon: ")
+
+
+print(f"Player: {player}")
+print(f"Computer: {computer}")
+
+
+if player == computer:
+ print("It's a tie!!")
+elif player == "rock" and computer == "scissors":
+ print("You WIN!!")
+elif player == "paper" and computer == "rock":
+ print("You WIN!!")
+elif computer == "scissors" and player == "paper":
+ print("You WIN!!")
+else:
+ print("You LOOSE!!")
