6f3b8b5a7a5186fe0429745799f01f129a2e8780
Rohit Jadhav <rj6228442@gmail.com>
2025-07-19 11:24:37 +0530

    added bet amount entering, exception handling and shows balance

--- Python_projects/Slot machine/slot_machine.py
+++ Python_projects/Slot machine/slot_machine.py
@@ -1,1 +1,37 @@
+def spin_row():
+  pass
 
+def print_row():
+  pass
+
+def get_payout():
+  pass
+
+balance = 100
+
+print("********************")
+print("Python slot machine ")
+print("Symbols: 🍒  🔔  ⭐")
+print("********************")
+
+while balance > 0:
+  print(f"Currrent balance is Rs.{balance}")
+
+  bet = input("Enter bet amount")
+
+  if not bet.isdigit():
+    print("Please enter a valid number")
+    continue
+
+  bet = int(bet)
+  
+  if bet > balance:
+    print("Insufficient funds")
+    continue
+  
+  if bet <= 0:
+    print("Bet must be greater than 0")
+    continue
+
+  balance -= bet
+  
