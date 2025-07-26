e7e22df80a02bba7297ba2fa4ebbdd68023bb93a
Rohit Jadhav <rj6228442@gmail.com>
2025-07-26 14:18:38 +0530

    plain text to encrypted text

--- Python_projects/Encryption program/encryption_prog.py
+++ Python_projects/Encryption program/encryption_prog.py
@@ -1,1 +1,16 @@
+import random
+import string
 
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+key = chars.copy()
+
+#ENCRYPT
+plain_text = input("Enter message to encrypt: ")
+cipher_text = ""
+
+for letter in plain_text:
+  index = chars.index(key)
+  cipher_text += key[index]
+print(f"Original text = {plain_text}")
+print(f"Encrypted text = {cipher_text}")
